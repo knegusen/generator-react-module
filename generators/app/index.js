b11a830492e3d417f1b42aeeb3aa754ac1982233
knegusen <knegusen@gmail.com>
2018-08-07 15:20:36 +0200

    Remove etraveli artifactory url

--- generators/app/index.js
+++ generators/app/index.js
@@ -34,7 +34,6 @@ module.exports = class extends Generator {
     this._copyFile('.gitattributes');
     this._copyFile('.gitignore');
     this._copyFile('.nontemplates');
-    this._copyFile('.npmrc');
     this._copyFile('changelog.md');
     this._copyFile('readme.md');
     this._copyFile('wallaby.js');
